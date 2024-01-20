6648658fecdfa7a8f4b7e4efe561b66653f007ea
Jonathon McDonald <jmcdonald.ut@gmail.com>
2024-01-20 16:48:33 -0700

    TMP 2 - More trees

--- src/Prelude/Tree.fs
+++ src/Prelude/Tree.fs
@@ -1,5 +1,7 @@
 module Tree
 
+open System.Collections.Generic
+
 /// The Tree type comprises a discriminated union with three types:
 ///
 /// - A branch node that both encodes data and subtrees.
@@ -14,24 +16,40 @@ type Node<'T> =
   | LeafNode of 'T
   | RootNode of seq<Node<'T>>
 
+type private SeqIndex<'T, 'K when 'K: equality>
+  (getNodeKey: ('T) -> 'K, tryGetParentNodeKey: ('T) -> option<'K>, nodes: seq<'T>) =
+  let state: IDictionary<option<'K>, seq<'T>> =
+    nodes |> Seq.groupBy tryGetParentNodeKey |> dict
+
+  member index.Roots() = Dict.propOr (seq []) None state
+
+  member index.TryGetNodeDescendants(node: 'T) =
+    match Dict.tryProp (Some(getNodeKey node)) state with
+    | None -> None
+    | Some(empty) when Seq.isEmpty empty -> None
+    | Some(descendants) -> Some(descendants)
+
+[<TailCall>]
+let rec private assembleDescendants (index: SeqIndex<'T, 'K>) k nodes acc =
+  let recurse = assembleDescendants index
+
+  match Seq.tryHead nodes with
+  | None -> k (Seq.rev acc)
+  | Some(nodeValue) ->
+    match index.TryGetNodeDescendants(nodeValue) with
+    | None -> recurse k (Seq.tail nodes) (Seq.insertAt 0 (LeafNode nodeValue) acc)
+    | Some(descendants) ->
+      recurse
+        (fun (descendants: seq<Node<'T>>) ->
+          recurse k (Seq.tail nodes) (Seq.insertAt 0 (BranchNode(nodeValue, descendants)) acc))
+        descendants
+        (seq [])
+
 let ofSeq (getNodeKey: ('T) -> 'K) (tryGetParentNodeKey: ('T) -> option<'K>) (nodes: seq<'T>) =
-  let index = nodes |> Seq.groupBy tryGetParentNodeKey |> dict
+  let index: SeqIndex<'T, 'K> =
+    new SeqIndex<'T, 'K>(getNodeKey, tryGetParentNodeKey, nodes)
 
-  let rootChildren = (Dict.propOr (seq []) None index) |> Seq.toList
-
-  let rec assembleDescendants produceCont (nodes: seq<'T>) acc =
-    match Seq.toList nodes with
-    | [] -> produceCont (List.rev acc)
-    | nodeValue :: tail ->
-      match Dict.tryProp (Some(getNodeKey nodeValue)) index with
-      | None -> assembleDescendants produceCont tail (LeafNode(nodeValue) :: acc)
-      | Some(children) ->
-        assembleDescendants
-          (fun descendants -> assembleDescendants produceCont tail (BranchNode(nodeValue, descendants) :: acc))
-          children
-          []
-
-  assembleDescendants (fun roots -> RootNode(roots)) rootChildren []
+  assembleDescendants index (fun (roots: seq<Node<'T>>) -> RootNode(roots)) (index.Roots()) (seq [])
 
 [<TailCall>]
 let rec private mapTreeCont cont mapper nodes acc =
@@ -46,7 +64,7 @@ let rec private mapTreeCont cont mapper nodes acc =
       []
   | RootNode(_) :: _ -> invalidArg "nodes" "Private func should never receive a root"
 
-let mapTree mapper root =
+let mapTree mapper (root: Node<'a>) =
   match root with
   | LeafNode(value) -> LeafNode(mapper value)
   | BranchNode(value, descendants) ->
@@ -62,16 +80,18 @@ type SampleData =
     Label: string }
 
 let makeSample () =
-  [ { Id = 1
-      ParentId = None
-      Label = "Root" }
-    { Id = 2
-      ParentId = Some(1)
-      Label = "Root -> L1(2)" }
-    { Id = 3
-      ParentId = Some(1)
-      Label = "Root -> L1(3)" }
-    { Id = 4
-      ParentId = Some(2)
-      Label = "root -> L1(2) -> L2(4)" } ]
-  |> ofSeq (_.Id) (_.ParentId)
+  let l: seq<SampleData> =
+    [ { Id = 1
+        ParentId = None
+        Label = "Root" }
+      { Id = 2
+        ParentId = Some(1)
+        Label = "Root -> L1(2)" }
+      { Id = 3
+        ParentId = Some(1)
+        Label = "Root -> L1(3)" }
+      { Id = 4
+        ParentId = Some(2)
+        Label = "root -> L1(2) -> L2(4)" } ]
+
+  ofSeq (_.Id) (_.ParentId) l
